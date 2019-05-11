bf8d784de4773e48b21799823112ff3684f49d92
Marko Cupic <m.cupic@gmx.ch>
2019-05-11 11:18:42 +0200

    dev

---
+++ src/Resources/contao/languages/de/tl_resource_booking.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Chronometry Module for Contao CMS
+ * Copyright (c) 2008-2019 Marko Cupic
+ * @package chronometry-bundle
+ * @author Marko Cupic m.cupic@gmx.ch, 2019
+ * @link https://github.com/markocupic/chronometry-bundle
+ */
+
+// Legends
+$GLOBALS['TL_LANG']['tl_resource_booking']['published_legend'] = 'Veröffentlichen';
+$GLOBALS['TL_LANG']['tl_resource_booking']['name_legend'] = 'Einstellungen Athlet';
+$GLOBALS['TL_LANG']['tl_resource_booking']['time_legend'] = 'Zeit-Einstellungen';
+
+// Fields
+$GLOBALS['TL_LANG']['tl_resource_booking']['pid'] = array('Ressource');
+$GLOBALS['TL_LANG']['tl_resource_booking']['timeSlotId'] = array('Zeitraum');
+$GLOBALS['TL_LANG']['tl_resource_booking']['member'] = array('Reserviert durch Mitglied');
+$GLOBALS['TL_LANG']['tl_resource_booking']['firstname'] = array('Vorname');
+$GLOBALS['TL_LANG']['tl_resource_booking']['lastname'] = array('Nachname');
+$GLOBALS['TL_LANG']['tl_resource_booking']['title'] = array('Titel');
+$GLOBALS['TL_LANG']['tl_resource_booking']['description'] = array('Beschreibung');
+$GLOBALS['TL_LANG']['tl_resource_booking']['startTime'] = array('Startzeit');
+$GLOBALS['TL_LANG']['tl_resource_booking']['endTime'] = array('Endzeit');
+$GLOBALS['TL_LANG']['tl_resource_booking']['endtime'] = array('Zielzeit');
+
+
