23eef7693612cf53c868851c7fb4d70912e4c28a
Marko Cupic <m.cupic@gmx.ch>
2022-06-05 14:28:32 +0200

    Update contao/easy-coding-standard to ver. 5.0

--- .ecs/config/default.php
+++ .ecs/config/default.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(__DIR__.'/../../../../contao/easy-coding-standard/config/default.php');
-    $containerConfigurator->import(__DIR__.'/set/header_comment_fixer.php');
+return static function (ECSConfig $ECSConfig): void {
+    $ECSConfig->import(__DIR__.'/../../../../contao/easy-coding-standard/config/default.php');
+    $ECSConfig->import(__DIR__.'/set/header_comment_fixer.php');
 };
