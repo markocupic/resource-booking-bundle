a0020d753771f7858e4438aa3af19dda48f4ca7f
Marko Cupic <m.cupic@gmx.ch>
2022-10-10 23:36:05 +0200

    Load backend assets via KernelRequestSubscriber

---
+++ src/EventSubscriber/KernelRequestSubscriber.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Resource Booking Bundle.
+ *
+ * (c) Marko Cupic 2022 <m.cupic@gmx.ch>
+ * @license MIT
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/markocupic/resource-booking-bundle
+ */
+
+namespace Markocupic\ResourceBookingBundle\EventSubscriber;
+
+use Contao\CoreBundle\Routing\ScopeMatcher;
+use Markocupic\ResourceBookingBundle\Config\RbbConfig;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class KernelRequestSubscriber implements EventSubscriberInterface
+{
+    protected $scopeMatcher;
+
+    public function __construct(ScopeMatcher $scopeMatcher)
+    {
+        $this->scopeMatcher = $scopeMatcher;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [KernelEvents::REQUEST => 'onKernelRequest'];
+    }
+
+    public function onKernelRequest(RequestEvent $e): void
+    {
+        $request = $e->getRequest();
+
+        if ($this->scopeMatcher->isBackendRequest($request)) {
+            $GLOBALS['TL_CSS'][] = RbbConfig::RBB_ASSET_PATH.'/css/backend.css';
+        }
+    }
+}
