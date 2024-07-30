c37e96644c5012aebb2c53346c33d056baa098bd
Marko Cupic <m.cupic@gmx.ch>
2024-07-30 23:54:34 +0200

    Update translation files

--- contao/languages/en/tl_module.php
+++ contao/languages/en/tl_module.php
@@ -13,13 +13,13 @@ declare(strict_types=1);
  */
 
 // Fields
-$GLOBALS['TL_LANG']['tl_module']['resourceBooking_resourceTypes'] = ['Resourcen-Types', 'Select one ore more resource types.'];
-$GLOBALS['TL_LANG']['tl_module']['resourceBooking_hideDays'] = ['Allow bookings for limited weekdays only', 'Allow booking for limited weekdays only.'];
-$GLOBALS['TL_LANG']['tl_module']['resourceBooking_hideDaysSelection'] = ['Select weekdays.', 'Select weekdays where booking will be possible.'];
-$GLOBALS['TL_LANG']['tl_module']['resourceBooking_addDateStop'] = ['Activate booking stop date', 'Activate booking stop date.'];
-$GLOBALS['TL_LANG']['tl_module']['resourceBooking_dateStop'] = ['Booking stop date', 'Select a booking stop date please.'];
-$GLOBALS['TL_LANG']['tl_module']['resourceBooking_displayClientPersonalData'] = ['Display personal data in the booking table', 'Display personal data in the booking table.'];
-$GLOBALS['TL_LANG']['tl_module']['resourceBooking_clientPersonalData'] = ['Select fields', 'Select fields.'];
-$GLOBALS['TL_LANG']['tl_module']['resourceBooking_setBookingSubmittedFields'] = ['Display booking details in the booking table', 'Display booking details in the booking table.'];
-$GLOBALS['TL_LANG']['tl_module']['resourceBooking_bookingSubmittedFields'] = ['Select fields', 'Select fields.'];
-$GLOBALS['TL_LANG']['tl_module']['resourceBooking_appConfig'] = ['App-Configuration', 'Create your custom app configuration in "app/config.yml".'];
+$GLOBALS['TL_LANG']['tl_module']['resourceBooking_resourceTypes'] = ['Resource types', 'Select at least 1 resource type'];
+$GLOBALS['TL_LANG']['tl_module']['resourceBooking_hideDays'] = ['Allow bookings only for selected days of the week', 'Specify whether bookings should only be allowed for selected days of the week'];
+$GLOBALS['TL_LANG']['tl_module']['resourceBooking_hideDaysSelection'] = ['Select days of the week', 'Select the days of the week on which bookings should be allowed'];
+$GLOBALS['TL_LANG']['tl_module']['resourceBooking_addDateStop'] = ['Set booking stop date', 'Activate the booking stop date'];
+$GLOBALS['TL_LANG']['tl_module']['resourceBooking_dateStop'] = ['Select booking stop date', 'Select a date until when bookings are possible'];
+$GLOBALS['TL_LANG']['tl_module']['resourceBooking_displayClientPersonalData'] = ['Display personal data of the booking person (tl_member)', 'Display personal data of the booking person in the table'];
+$GLOBALS['TL_LANG']['tl_module']['resourceBooking_clientPersonalData'] = ['Select fields (tl_member)', 'Select the fields to be displayed. Template customization may be necessary!'];
+$GLOBALS['TL_LANG']['tl_module']['resourceBooking_setBookingSubmittedFields'] = ['Show booking details in the table (tl_resource_booking)', 'Show booking details in the table'];
+$GLOBALS['TL_LANG']['tl_module']['resourceBooking_bookingSubmittedFields'] = ['Select booking details fields (tl_resource_booking)', 'Select the fields to be displayed. Template customization may be necessary!'];
+$GLOBALS['TL_LANG']['tl_module']['resourceBooking_appConfig'] = ['Select app configuration', 'Create your custom app configuration in “config/config.yaml”'];
